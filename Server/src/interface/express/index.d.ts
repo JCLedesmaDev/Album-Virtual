5ae5351a5f62cb2c19f05eaa9172d8379ce99c4b
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2023-01-21 13:56:23 -0300

    Correcciones de headersHandler

--- Server/src/interface/express/index.d.ts
+++ Server/src/interface/express/index.d.ts
@@ -1,16 +1,27 @@
-interface Ilocals {
-  usrId: string;
-  usrToken: string;
-  usrRoles: IRol[];
-  
-  mockmode: string;
-  info: any;
-  result: any;
-  finished: boolean
-}
 
 declare namespace Express {
-  interface Request {
-    locals: Ilocals;
+  interface Request extends IRol {
+    /**
+     * Locals posee los sig. campos:
+     * @field usrId: string;
+     * @field usrToken: string;
+     * @field usrRoles: IRol[];
+     * @field mockmode: string;
+     * @field info: any;
+     * @field result: any;
+     * @field finished: boolean
+    */
+    locals: {
+      usrId: string;
+      usrToken: string;
+      usrRoles: {
+        name: string;
+        id: string; 
+      }[];
+      mockmode: string;
+      info: any;
+      result: any;
+      finished: boolean
+    };
   }
 }
--- Server/src/middlewares/headersHandler.ts
+++ Server/src/middlewares/headersHandler.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from "express"
+import { ApplicationError } from "../utils/applicationError"
+
 
 const headersHandler = (req: Request, res: Response, next: NextFunction) => {
 
+    req.locals = {} as any
+
     const usrToken = (req.headers['authorization']) ? req.headers['authorization'].toString().toUpperCase() : ''
     req.locals.usrToken = usrToken
-        
+
     const mockmode = (req.headers['mockmode'] === 'true') ? 'true' : 'false'
     req.locals.mockmode = mockmode
-    
+
     const usrId = req.headers['userId'] ? req.headers['userId'].toString().toUpperCase() : ''
     req.locals.usrId = usrId
 
