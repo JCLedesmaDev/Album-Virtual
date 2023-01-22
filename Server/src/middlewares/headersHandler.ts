393bea557edff74d3ce0820bfdd1e2826c3f2377
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-01-21 21:06:04 -0300

    corr

--- Server/src/middlewares/headersHandler.ts
+++ Server/src/middlewares/headersHandler.ts
@@ -4,13 +4,13 @@ const headersHandler = (req: Request, res: Response, next: NextFunction) => {
 
     req.locals = {} as any
 
-    const usrToken = (req.headers['authorization']) ? req.headers['authorization'].toString().toUpperCase() : ''
+    const usrToken = (req.headers['authorization']) ? req.headers['authorization'].toString() : ''
     req.locals.usrToken = usrToken
 
     const mockmode = (req.headers['mockmode'] === 'true') ? 'true' : 'false'
     req.locals.mockmode = mockmode
 
-    const usrId = req.headers['userId'] ? req.headers['userId'].toString().toUpperCase() : ''
+    const usrId = (req.headers['userid']) ? req.headers['userid'].toString() : ''
     req.locals.usrId = usrId
 
     next()
