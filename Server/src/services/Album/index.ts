b86fac3973a5589e6536446e44cd12600a0371f9
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2023-01-22 13:40:59 -0300

    borrado de codigo innecesario

--- Server/src/services/Album/index.ts
+++ Server/src/services/Album/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { asdasd } from './controller'
 import { checkRolesHandler } from '../../middlewares/checkRolesHandler'
 import { mockHandler } from "../../middlewares/mockHandler";
@@ -7,7 +7,7 @@ import { authHandler } from "../../middlewares/authHandler";
 const router = express.Router();
 
 router.use(authHandler)
-// router.use(mockHandler)
+router.use(mockHandler)
 
 /** 
  * Obtener albumes
@@ -27,36 +27,6 @@ router.use(authHandler)
  *       - idSecurity: []
  */
 router.get('/', checkRolesHandler(['Admin']), asdasd)
-// export {...controllers }
-export default router
 
-/*
-/** 
- * Obtener albumes
- * @swagger
- * /{id}:
- *    get:
- *      tags: [album]  
- *      summary: "Listar AAAAA"
- *      description: Este endpoint es para listar los usuario totales.
- *      parameters:
- *        - name: id
- *          in: path
- *          description: ID of pet to return
- *          required: true
- *          schema:
- *            type: integer
- *            format: int64    
- *      requestBody:
- *          content:
- *            application/json:
- *              schema:
- *                $ref: "#/components/schemas/user"
- *      responses:
- *        '200':
- *          description: Retorna el objeto insertado en la coleccion.
- *        '422':
- *          description: Error de validacion.
- *      security:
- *       - bearerAuth: []
- */ 
+
+export default router
