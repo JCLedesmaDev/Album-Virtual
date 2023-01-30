d3cf2632673fe2bb02526f462f98aba643569169
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-01-30 17:58:01 -0300

    Figurites router

--- Server/src/routes/index.routes.ts
+++ Server/src/routes/index.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import UsersRoutes from '../services/users'
-import AlbumesRoutes from '../services/albumes' //Nota, hace referencia al index de la carpeta "Album" donde tenemos los http definidos
-
+import AlbumesRoutes from '../services/albumes'
+import FiguritesRoutes from '../services/figurites'
 const router = express.Router();
 
 router.use('/albumes', AlbumesRoutes)
 router.use('/users', UsersRoutes)
+router.use('/figurites', FiguritesRoutes)
+// router.use('/albumCollections', FiguritesRoutes)
 
 
 export default router;
