0d882ff70d6a38a074ce3c58e402badf2d54d88e
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-02-03 09:21:10 -0300

    Creacion de servicio collection

--- Server/src/services/albumCollections/controller.ts
+++ Server/src/services/albumCollections/controller.ts
@@ -1,1 +1,20 @@
-a
+import { Request, Response, NextFunction } from "express"
+import { matchedData } from "express-validator"
+import { IPagination } from "../../interface/IPagination"
+import logic from './logic'
+
+const createCollection = async (req: Request, res: Response, next: NextFunction) => {
+}
+const getAllCollection = async (req: Request, res: Response, next: NextFunction) => {
+}
+const deleteCollection = async (req: Request, res: Response, next: NextFunction) => {
+}
+const updateCollection = async (req: Request, res: Response, next: NextFunction) => {
+}
+
+export {
+    createCollection, 
+    getAllCollection, 
+    deleteCollection,
+    updateCollection
+}
--- Server/src/services/albumCollections/index.ts
+++ Server/src/services/albumCollections/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { checkRolesHandler } from '../../middlewares/checkRolesHandler'
 import { mockHandler } from "../../middlewares/mockHandler";
 import { authHandler } from "../../middlewares/authHandler";
+import { createCollection, deleteCollection, getAllCollection, updateCollection } from "./controller";
 
 const router = express.Router();
 
@@ -9,13 +10,13 @@ router.use(authHandler)
 router.use(mockHandler)
 
 
-router.post('/createCollection', checkRolesHandler(), validatorCreateAlbum, createAlbum)
+router.post('/createCollection', checkRolesHandler(), validatorCreateAlbum, createCollection)
 
-router.get('/getAllCollection', checkRolesHandler(['User']), getListAlbumes)
+router.get('/getAllCollection', checkRolesHandler(['User']), getAllCollection)
 
-router.delete('/deleteCollection/:id', checkRolesHandler(), validatorDeleteAlbum, deleteAlbum)
+router.delete('/deleteCollection/:id', checkRolesHandler(), validatorDeleteAlbum, deleteCollection)
 
-router.put('/updateCollection/:id', checkRolesHandler(), validatorUpdateAlbum, updateAlbum)
+router.put('/updateCollection/:id', checkRolesHandler(), validatorUpdateAlbum, updateCollection)
 
 
 export default router
