53c42d3d25e7804fdc2b09c3c2f3f049710fade2
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2023-02-04 16:35:52 -0300

    Comienzo de front

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <GlobalProvider>
+        <Navigate />
+
+        <Routes>
+          <Route path="/" element={
+            <AuthProvider>
+              <Authentication />
+            </AuthProvider>
+          } />
+
+          <Route path="/AlbumUsuario" element={
+            <RoutePrivate>
+              <AlbumUsuario />
+            </RoutePrivate>
+          } />
+
+          <Route path="/AlbumUsuarioImagen/:nombreAlbum/:id" element={
+            <RoutePrivate>
+              <AlbumUsuarioImagen />
+            </RoutePrivate>
+          } />
+
+          <Route path="/AlbumImagenes" element={
+            <RoutePrivate>
+              <AlbumImagenes />
+            </RoutePrivate>
+          } />
+
+          <Route path="/Album" element={
+            <RoutePrivate>
+              <Album />
+            </RoutePrivate>
+          } />
+
+          <Route path="/AdminAlbum" element={
+            <RoutePrivate>
+              <AdminAlbum />
+            </RoutePrivate>
+          } />
+          <Route path="/AdminCollection" element={
+            <RoutePrivate>
+              <AdminCollection />
+            </RoutePrivate>
+          } />
+
+          <Route path="/AdminFigurita" element={
+            <RoutePrivate>
+              <AdminFiguritas />
+            </RoutePrivate>
+          } />
+
+        </Routes>
+
+        <ModalLoader />
+
+        <ModalStatus />
+
+      </GlobalProvider>
+    </>
   )
 }
 
-export default App
+const AppWrapper = () => (
+  // Englobamos Redux al proyecto
+  // <Provider store={store}>
+  <App />
+  /* </Provider> */
+);
+export default AppWrapper
