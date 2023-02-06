4aed494affb7357f517964bc48db295cb26da67a
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2023-02-05 22:59:51 -0300

    .

---
+++ Client/src/mappers/User.mappers.ts
@@ -0,0 +1,14 @@
+import { IAuthData } from "../interface/DTO Back/Auth/IAuthData";
+import { UserModels } from "../Models/User.models";
+
+export const createMapperUser = (AuthData: IAuthData): UserModels => {
+
+    const formattedUser: UserModels = {
+        Id: AuthData.user?.id,
+        FullName: AuthData.user?.nombreCompleto,
+        Email: AuthData.user?.email,
+        Token: AuthData.token,
+    };
+
+    return formattedUser;
+};
