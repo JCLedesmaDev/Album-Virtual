bfd390485017aaa4b6cc436f51ad8982237c673f
Juan Cruz Ledesma <g-ohan2009@hotmail.com>
2023-02-06 22:10:00 -0300

    Exploracion de react-router-dom 6.4

---
+++ Client/src/layout/MainLayout.tsx
@@ -0,0 +1,36 @@
+import { Outlet,  useNavigation, useParams } from "react-router-dom"
+import { ModalLoader } from "../components/ModalLoader/ModalLoader"
+import { ModalStatus } from "../components/ModalStatus/ModalStatus"
+import { Navigate } from "../components/Navigate/Navigate"
+
+export const MainLayout: React.FC = () => {
+
+    const navigation = useNavigation() 
+    /* Devuelve el estado de la navegacion actual
+        - idle, submitting, loading
+    */
+    // useParams()
+    
+    return (
+        <>
+            <Navigate />
+
+            <main>
+                
+                {
+                    navigation.state === 'loading' && (
+                        <h1>Cargando pagina... Espere</h1>
+                    )
+                }
+                
+                {/* Gracais al Outlet aqui se plasmaran todos los childrens de router/index.tsx */}
+                <Outlet />
+            </main>
+
+            <ModalLoader />
+            <ModalStatus />
+            
+            <footer>Foooter</footer>
+        </>
+    )
+}
--- Client/src/pages/NotFound/index.tsx
+++ Client/src/pages/NotFound/index.tsx
@@ -1,14 +1,19 @@
-import { useRouteError } from "react-router-dom"
+import { Link, useRouteError } from "react-router-dom"
 
 
 
 export const NotFound: React.FC = () => {
 
-    const error = useRouteError()
+    const error = useRouteError() as any
     console.log("🚀 ~ file: index.tsx:8 ~ error", error)
 
 
     return (
-        <h1>'Not found'</h1>
+        <div>
+            <h1>404</h1>
+            <p>Page not found</p>
+            <p>{error.statusText || error.message}</p>
+            <Link to="/">Volver al HOme</Link>
+        </div>
     )
 }
