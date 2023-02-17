9f11d673cb26dea7d234c8265556c32761239ba5
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-02-17 12:57:20 -0300

    fix routes albumCollections

--- Server/src/routes/index.routes.ts
+++ Server/src/routes/index.routes.ts
@@ -2,12 +2,13 @@ import express from "express";
 import UsersRoutes from '../services/users'
 import AlbumesRoutes from '../services/albumes'
 import FiguritesRoutes from '../services/figurites'
+import AlbumCollectionRoutes from '../services/albumCollections'
 const router = express.Router();
 
 router.use('/albumes', AlbumesRoutes)
 router.use('/users', UsersRoutes)
 router.use('/figurites', FiguritesRoutes)
-router.use('/albumCollections', FiguritesRoutes)
+router.use('/albumCollections', AlbumCollectionRoutes)
 
 
 export default router;
