cc5b55a9fec8e3dcddb2f7399a629374140b7e6a
Juan Cruz Ledesma <juancruzledesma@yahoo.com.ar>
2023-03-09 14:18:39 -0300

    Back - ControllerWrapper.ts

--- Server/src/utils/controllerWrapper.ts
+++ Server/src/utils/controllerWrapper.ts
@@ -2,20 +2,16 @@ import { NextFunction, Request, Response } from "express";
 
 const controllerWrapper = (callback: any) => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const data: any = await callback(req)
+        const data: any = await callback(req)
 
-            req.locals.result = data // Se utiliza en el eventHandler
+        req.locals.result = data // Se utiliza en el eventHandler
 
-            // SI es true, pasa directo hacia errorHandler
-            if (data?.error) return next(data.error)
+        // SI es true, pasa directo hacia errorHandler
+        if (data?.error) return next(data.error)
 
-            req.locals.finished = true
-            res.json(data)
-            next()
-        } catch (error: any) {
-            return { error }
-        }
+        req.locals.finished = true
+        res.json(data)
+        next()
     }
 }
 
